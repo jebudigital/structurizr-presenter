b92d17d3ff81403d89cc626cd884d0476d564cc9
Jebu Martin <jebudigital@gmail.com>
2026-06-28 23:18:46 +0530

    chore: TypeScript + ESLint + Prettier config

---
+++ eslint.config.js
@@ -0,0 +1,18 @@
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import prettier from "eslint-config-prettier";
+
+export default tseslint.config(
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  prettier,
+  {
+    ignores: ["**/dist/**", "**/node_modules/**", "**/*.js.map", "packages/runtime/vite.config.ts"],
+  },
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+      "@typescript-eslint/consistent-type-imports": "error",
+    },
+  }
+);
